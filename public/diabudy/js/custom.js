db0cb5e0b482bd2eaa63ee678571ad7561c1ad0f
Usman Akram <usman.akram@gmail.com>
2018-05-04 00:40:34 +0500

    diabudy theme

---
+++ public/diabudy/js/custom.js
@@ -0,0 +1,22 @@
+/* Add your custom JavaScript code */
+
+function notify_success(msg) {
+    $.notify({
+        title: '<strong>Heads up!</strong>',
+        message: msg
+    },{
+        type: 'success',
+        animate: {
+            enter: 'animated bounceIn',
+            exit: 'animated bounceOut'
+        }
+    });
+}
+function notify_error(msg) {
+    $.notify({
+        title: '<strong>Heads up!</strong>',
+        message: msg
+    },{
+        type: 'error'
+    });
+}
